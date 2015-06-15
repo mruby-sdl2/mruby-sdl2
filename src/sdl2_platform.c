22ea9671e44467a2d4e976482ce3d400e79167b4
Moon4u <mooner4u@gmail.com>
2015-06-15 17:38:34 +0300

    Platform implementation

---
+++ src/sdl2_platform.c
@@ -0,0 +1,23 @@
+#include "sdl2_platform.h"
+
+
+
+static mrb_value
+mrb_sdl2_platform_get_platform(mrb_state *mrb, mrb_value self)
+{
+  return mrb_str_new_cstr(mrb, SDL_GetPlatform());
+}
+
+void
+mruby_sdl2_platform_init(mrb_state *mrb)
+{
+  struct RClass * mod_Platform = mrb_define_module_under(mrb, mod_SDL2, "Platform");
+  mrb_define_module_function(mrb, mod_Platform, "get_platform", mrb_sdl2_platform_get_platform,  ARGS_NONE());
+}
+
+
+
+void
+mruby_sdl2_platform_final(mrb_state *mrb)
+{
+}
---
+++ src/sdl2_platform.h
@@ -0,0 +1,20 @@
+#ifndef MRUBY_SDL2_PLATFORM_H
+#define MRUBY_SDL2_PLATFORM_H
+
+#include "sdl2.h"
+#include <SDL2/SDL_platform.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern struct RClass *mod_Mouse;
+
+extern void mruby_sdl2_platform_init(mrb_state *mrb);
+extern void mruby_sdl2_platform_final(mrb_state *mrb);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* end of MRUBY_SDL2_MOUSE_H */

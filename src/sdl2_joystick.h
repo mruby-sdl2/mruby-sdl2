17b2aff0429dab92ed546f491301091d40287b6e
Moon4u <mooner4u@gmail.com>
2015-06-24 00:23:36 +0300

    bug fixes

--- src/sdl2_joystick.h
+++ src/sdl2_joystick.h
@@ -9,6 +9,7 @@ extern "C" {
 #endif
 extern struct RClass *class_Joystick;
 extern SDL_Joystick * mrb_sdl2_joystick_joystick_get_ptr(mrb_state *mrb, mrb_value joystick);
+extern mrb_value mrb_sdl2_joystick_joystick(mrb_state *mrb, SDL_Joystick *joystick);
 extern void mruby_sdl2_joystick_init(mrb_state *mrb);
 extern void mruby_sdl2_joystick_final(mrb_state *mrb);
 
--- src/sdl2_rwops.h
+++ src/sdl2_rwops.h
@@ -8,6 +8,7 @@
 extern "C" {
 #endif
 
+extern SDL_RWops * mrb_sdl2_rwops_get_ptr(mrb_state *mrb, mrb_value rwops);
 extern void mruby_sdl2_rwops_init(mrb_state *mrb);
 extern void mruby_sdl2_rwops_final(mrb_state *mrb);
 

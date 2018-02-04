c441d0b737cc1c8bacf2414c7e319026458bfca8
ecirmoa <ecirmoa@gmail.com>
2018-02-04 15:55:15 +0900

    sample/input: fix. remove cursor setting.

--- samples/input.rb
+++ samples/input.rb
@@ -22,99 +22,58 @@ def windowevent_to_s(event)
   end
 end
 
-# create cursor from string.
-cursor_s = <<CURSOR
-XXXXXXXXXX                     
-X........X                     
-X.......XX                     
-X......XX                      
-X.....XX                       
-X....XX                        
-X...XX                         
-X..XX                          
-X.XX                           
-XXX                            
-CURSOR
-
-# cursor data is mapped to each bits.
-# "32 * 4" means that "32 pixels * 4byte * 8bits = 32 pixels * 32 pixels".
-cursor_data = SDL2::ByteBuffer.new(32 * 4)
-cursor_mask = SDL2::ByteBuffer.new(32 * 4)
-
-puts cursor_s.length
-i = 0
-byte_index = 0;
-while i < cursor_s.length
-  if (i % 8) != 0 then
-    cursor_data[byte_index] = cursor_data[byte_index] << 1
-    cursor_mask[byte_index] = cursor_mask[byte_index] << 1
-  else
-    byte_index += 1
-    cursor_data[byte_index] = 0
-  end
-  case cursor_s[i]
-    when 'X' then
-      cursor_data[byte_index] |= 1
-      cursor_mask[byte_index] |= 1
-    when '.' then
-      cursor_data[byte_index] |= 0
-      cursor_mask[byte_index] |= 1
-    else
-      cursor_data[byte_index] |= 0
-      cursor_mask[byte_index] |= 0
-  end
-  i += 1
-end
-
 SDL2::init
 
 begin
   SDL2::Video::init
   begin
     w = SDL2::Video::Window.new "sample", 100, 100, 640, 480, SDL2::Video::Window::SDL_WINDOW_SHOWN
-    c = SDL2::Input::Mouse::Cursor.new(cursor_data.cptr, cursor_mask.cptr, 32, 32, 0, 0)
-    SDL2::Input::Mouse::cursor = c
-    puts c
+
     renderer = SDL2::Video::Renderer.new(w)
-    renderer.draw_color = SDL2::RGB.new(0, 0, 0)
+    renderer.set_draw_color(0, 0, 0, 0)
     renderer.clear
-    renderer.draw_color = SDL2::RGB.new(0xff, 0xff, 0xff)
+    renderer.set_draw_color(0xff, 0xff, 0xff, 0xFF)
     old_p = nil
     render_on = false
-    until (ev = SDL2::Input::wait).nil?
-      puts case ev.type
-      when SDL2::Input::SDL_KEYDOWN then
-        "down: #{scancode_to_s(ev.keysym.scancode)}"
-      when SDL2::Input::SDL_KEYUP then
-        "up: #{scancode_to_s(ev.keysym.scancode)}"
-      when SDL2::Input::SDL_WINDOWEVENT then
-        "window: #{windowevent_to_s(ev.event)}"
-      when SDL2::Input::SDL_MOUSEMOTION then
-        if render_on then
-          if old_p.nil? then
-            old_p = SDL2::Point.new(ev.x, ev.y)
-          else
-            renderer.draw_line(old_p, SDL2::Point.new(ev.x, ev.y))
-            old_p = SDL2::Point.new(ev.x, ev.y)
+
+    running = true
+    while running
+      while ev = SDL2::Input::poll()
+        puts case ev.type
+        when SDL2::Input::SDL_KEYDOWN then
+          "down: #{scancode_to_s(ev.keysym.scancode)}"
+        when SDL2::Input::SDL_KEYUP then
+          "up: #{scancode_to_s(ev.keysym.scancode)}"
+        when SDL2::Input::SDL_WINDOWEVENT then
+          "window: #{windowevent_to_s(ev.event)}"
+        when SDL2::Input::SDL_MOUSEMOTION then
+          if render_on then
+            if old_p.nil? then
+              old_p = SDL2::Point.new(ev.x, ev.y)
+            else
+              renderer.draw_line(old_p, SDL2::Point.new(ev.x, ev.y))
+              old_p = SDL2::Point.new(ev.x, ev.y)
+            end
           end
+          "mouse motion: #{ev.x}, #{ev.y}"
+        when SDL2::Input::SDL_MOUSEBUTTONDOWN then
+          render_on = true
+          old_p = nil
+          "mouse button down: #{ev.x}, #{ev.y}"
+        when SDL2::Input::SDL_MOUSEBUTTONUP then
+          render_on = false
+          old_p = nil
+          "mouse button up: #{ev.x}, #{ev.y}"
+        when SDL2::Input::SDL_QUIT then
+          "quit"
+        else
+          "?: #{ev.to_s}"
         end
-        "mouse motion: #{ev.x}, #{ev.y}"
-      when SDL2::Input::SDL_MOUSEBUTTONDOWN then
-        render_on = true
-        old_p = nil
-        "mouse button down: #{ev.x}, #{ev.y}"
-      when SDL2::Input::SDL_MOUSEBUTTONUP then
-        render_on = false
-        old_p = nil
-        "mouse button up: #{ev.x}, #{ev.y}"
-      when SDL2::Input::SDL_QUIT then
-        "quit"
-      else
-        "?: #{ev.to_s}"
+        if ev.type == SDL2::Input::SDL_QUIT then
+          running = false
+        end
       end
-      if ev.type == SDL2::Input::SDL_QUIT then
-        break
-      end
+      SDL2::delay 20
       renderer.present
     end
     renderer.destroy

0406ff31f134c16ee6754deb45e64304b0c4920d
ecirmoa <ecirmoa@gmail.com>
2018-02-04 15:56:51 +0900

    samples/simple-window: window open and close.

---
+++ samples/simple-window.rb
@@ -0,0 +1,22 @@
+SDL2::init
+SDL2::Video::init
+width = 320
+height = 240
+x = SDL2::Video::Window::SDL_WINDOWPOS_UNDEFINED
+y = SDL2::Video::Window::SDL_WINDOWPOS_UNDEFINED
+flag = 0
+
+#
+window = SDL2::Video::Window.new "sample", x,y, width,height, flag
+p [window.title,  window.width, window.height]
+
+loop do
+  while ev = SDL2::Input::poll()
+    if ev.type == SDL2::Input::SDL_KEYDOWN
+      window.set_size 160,120
+    end
+    if ev.type == SDL2::Input::SDL_QUIT
+      exit(1)
+    end
+  end
+end
